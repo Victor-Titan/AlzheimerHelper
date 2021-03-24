bd06c3b37ba79020b94de4778d650295780ecd8b
Victor-Titan <vrajgandhi@hotmail.com>
2021-03-24 22:03:45 +0530

    Added more convo scenarios and a readme

---
+++ server.py
@@ -0,0 +1,11 @@
+import flask
+
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def handle_request():
+    return "Successful Connection"
+
+app.run(host="0.0.0.0", port=5000, debug=True)
