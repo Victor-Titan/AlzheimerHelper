6a064adbb4006a4c3d3a9e327a992b8f4a5976e7
Arjav Garg <arjavgarg@gmail.com>
2021-03-25 05:35:45 +0530

    Sending bot responses now

--- server.py
+++ server.py
@@ -1,12 +1,31 @@
 from flask import request, Flask
-import main
+import aiml
+import os
 
 app = Flask(__name__)
+kernel = aiml.Kernel()
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile = "bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
+    kernel.saveBrain("bot_brain.brn")
+
+def register(name, address, phno, dad, mom, emcon, guardian, age):
+    kernel.setPredicate('name', name)
+    kernel.setPredicate('address', address)
+    kernel.setPredicate('phno', phno)
+    kernel.setPredicate('fname', dad)
+    kernel.setPredicate('mom', mom)
+    kernel.setPredicate('emcon', emcon)
+    kernel.setPredicate('guardian', guardian)
+    kernel.setPredicate('age', age)
 
 @app.route('/', methods=['GET', 'POST'])
 def handle_request():
     if request.args['type'] == 'register':
-        main.register(request.args['name'], request.args['address'], request.args['phno'], request.args['dad'], request.args['mom'], request.args['emcon'], request.args['guardian'], request.args['age']);
+        register(request.args['name'], request.args['address'], request.args['phno'], request.args['dad'], request.args['mom'], request.args['emcon'], request.args['guardian'], request.args['age']);
+    elif request.args['type'] == 'response':
+        return kernel.respond(request.args['message'])
     return "something"
 
 app.run(host="0.0.0.0", port=5000, debug=True)
